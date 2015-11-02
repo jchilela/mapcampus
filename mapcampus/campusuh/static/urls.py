0ac4efbe5d13cac764246d32cddf79a561f8882d
root <root@ip-172-31-19-182.us-west-2.compute.internal>
2015-11-02 15:56:48 +0000

    02-11-2015

---
+++ mapcampus/campusuh/static/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', 'campusuhapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
