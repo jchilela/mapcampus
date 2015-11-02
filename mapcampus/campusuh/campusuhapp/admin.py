cc490d90fc5901d72c9b6bb50480d8a1ed285839
Júlio Gabriel Chilela <juliogabrielchilela1@MacBook-Pro-de-Julio.local>
2015-11-02 12:17:09 -0600

    adin file

--- mapcampus/campusuh/campusuhapp/admin.py
+++ mapcampus/campusuh/campusuhapp/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Users,Objects,Camera,Coordenates,Servers,MapSettings,ObjectsType
+from .models import ObjectsType,Users,Objects,Camera,ObjectsCoordenates,Servers,MapSettings,Buildings,Floors,Rooms,FloorsCoordinates,RoomsCoordinates
 
 
 # Register your models here.
+admin.site.register(ObjectsType)
 admin.site.register(Users)
 admin.site.register(Objects)
 admin.site.register(Camera)
-admin.site.register(Coordenates)
+admin.site.register(ObjectsCoordenates)
 admin.site.register(Servers)
 admin.site.register(MapSettings)
-admin.site.register(ObjectsType)
+admin.site.register(Buildings)
+admin.site.register(Floors)
+admin.site.register(Rooms)
+admin.site.register(FloorsCoordinates)
+admin.site.register(RoomsCoordinates)
